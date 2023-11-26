a03c34736693ee5b3159b4dca593ee3c8a69364e
mike <orion_mike@mail.ru>
2023-11-26 14:45:14 +0300

    add views

--- blog/urls.py
+++ blog/urls.py
@@ -1,4 +1,10 @@
 from django.urls import path
 
+from .views import *
+
+
 urlpatterns = [
+    path('', post_list, name='post_list'),
+    path('<slug:slug>/', post_detail, name='post_detail'),
+    path('b/<slug:slug>/', category_detail, name='category_detail'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,89 @@
-from django.shortcuts import render
 
-# Create your views here.
+from django.shortcuts import get_object_or_404, render
+
+from _utils.utils import get_pagination
+from config.config import PAGINATE_BY
+
+from .models import Category, Post
+
+
+def search_post_list(request):
+
+    search = request.GET.get('search', '')
+
+    if search:
+        post_list = Post.objects.filter(title__icontains=search, is_publish=True)
+        post_list, paginator, is_paginated, prev_url, next_url = get_pagination(request, post_list, PAGINATE_BY)
+
+    else:
+        post_list = Post.objects.filter(is_publish=True)
+        object_list, paginator, is_paginated, prev_url, next_url = get_pagination(request, post_list, PAGINATE_BY)
+
+    return render(
+        request,
+        'blog/post_list.html',
+        context={
+            'object_list': object_list,
+            'is_paginated': is_paginated,
+            'next_page': next_url,
+            'prev_page': prev_url,
+        })
+
+
+def post_list(request):
+
+    post_list = Post.objects.filter(is_publish=True)
+    object_list, paginator, is_paginated, prev_url, next_url = get_pagination(request, post_list, PAGINATE_BY)
+
+    return render(
+        request,
+        'blog/post_list.html',
+        context={
+            'object_list': object_list,
+            'is_paginated': is_paginated,
+            'next_page': next_url,
+            'prev_page': prev_url,
+        })
+
+
+def post_detail(request, slug):
+
+    post = get_object_or_404(Post, slug=slug)
+
+    post_next = Post.objects.filter(date_update__gt=post.date_update).order_by('date_update').first()
+    post_previos = Post.objects.filter(date_update__lt=post.date_update).order_by('-date_update').first()
+
+    related_post_list = Post.objects.filter(
+        is_publish=True,
+        category_id=post.category).order_by('?').exclude(pk=post.pk)[:6]
+
+    return render(
+        request,
+        'blog/post_detail.html',
+        context={
+            'post': post,
+
+            'post_next': post_next,
+            'post_previos': post_previos,
+
+            'related_post_list': related_post_list
+        })
+
+
+def category_detail(request, slug):
+
+    category = Category.objects.filter(slug=slug).first()
+
+    post_list = Post.objects.filter(category_id=category.id).all()
+    object_list, paginator, is_paginated, prev_url, next_url = get_pagination(request, post_list, PAGINATE_BY)
+
+    return render(
+        request,
+        'blog/category_detail.html',
+        context={
+            'category': category,
+            'object_list': object_list,
+            'is_paginated': is_paginated,
+            'next_page': next_url,
+            'prev_page': prev_url,
+        })
